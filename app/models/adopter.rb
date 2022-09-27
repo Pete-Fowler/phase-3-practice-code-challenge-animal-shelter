5cacd40501739fa07d2237a6689ba814f43b3414
Pete Fowler <fowler.pete@gmail.com>
2022-09-27 17:06:16 -0600

    Add solution

--- app/models/adopter.rb
+++ app/models/adopter.rb
@@ -1,4 +1,21 @@
 class Adopter < ActiveRecord::Base
   has_many :pets
-  has_many :shelters, through: :pets
+  has_many :shelters, through: :pets 
+
+  def full_name 
+    "#{first_name} #{last_name}" 
+  end 
+
+  def adopt(pet) 
+    if pet.adopted? == false
+      pet.update(adopted?: true, adopter_id: id)
+    else
+      puts "Pet is already adopted"
+    end 
+  end 
+
+  def fav_pet
+    self.pets.maximum(:species)
+  end 
+
 end
