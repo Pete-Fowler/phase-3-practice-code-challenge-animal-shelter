8e31a0fef5fc363c562f877a79bff6dc6739e8fe
Pete Fowler <fowler.pete@gmail.com>
2022-09-27 15:55:47 -0600

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,14 @@ puts "Seeding..."
   )
 end
 
-10.times do 
+20.times do 
   Pet.create(
     name: Faker::Name.name, 
     age: Faker::Number.between(from: 1, to: 10), 
-    species: Faker::Creature.Animal.name, 
+    species: Faker::Creature::Animal.name, 
     adopted?: Faker::Boolean.boolean, 
-    shelter_id: Faker::Number.unique.between(from:1, to: 5), 
-    adopter_id: Faker::Number.unique.between(from:1, to: 5)
+    shelter_id: rand(1..10), 
+    adopter_id: rand(1..10) 
   )
-end 
 
 end 
