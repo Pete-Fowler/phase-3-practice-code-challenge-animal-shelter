452666ba9326d3a4ef6319eef5b8a9eda2e2d1b0
Pete Fowler <fowler.pete@gmail.com>
2022-09-27 16:29:51 -0600

    Shelter methods complete

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,5 +1,13 @@
 class Shelter < ActiveRecord::Base
   has_many :pets 
   has_many :adopters, through: :pets
+
+  def current_pets
+    self.pets.where(adopted?: false)
+  end 
+
+  def adopted_pets
+    pets.where(adopted?: true)
+  end 
 end
  
